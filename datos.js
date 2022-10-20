ff11f7aa642f7223a55bda518dd92f8f0694f789
Karen Ordaz <107648016+karen2807@users.noreply.github.com>
2022-10-20 12:22:38 -0400

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,16 @@
+
+
+const dato ={
+    nombre: 'Karen',
+    edad: 27,
+    desarrollador: true,
+    fecha_nac: new Date(1995,6, 28),
+    libro_fav: {
+        titulo: 'La reina roja',
+        autor: 'Victoria Aveyard',
+        fecha_lib: new Date(2016, 3, 1),
+        url: 'milibro.com'
+    }
+}
+
+console.log(dato);
